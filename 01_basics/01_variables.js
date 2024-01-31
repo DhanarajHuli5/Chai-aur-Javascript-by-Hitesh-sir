47d91fe5395012d5052a9a1660e6316636faf52b
DhanarajHuli5 <dhanarajhuli5@gmail.com>
2024-01-31 20:09:34 +0530

    Basics 01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaiput"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountEmail]);
